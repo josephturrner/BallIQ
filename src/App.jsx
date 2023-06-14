60699d2b5a9cb702adad71d0f889375cd076ea09
sephturner <turnerj8@wit.edu>
2023-06-14 12:57:29 -0400

    Initial routing & file structure

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from 'react-router-dom'
+
+// layouts and pages
+import RootLayout from './layouts/RootLayout'
+import Home from './pages/Home'
+import Stats from './pages/Stats'
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="stats" element={<Stats />} />
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
