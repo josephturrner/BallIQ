49c615b1f7f90577ecf39e2f7691f346de44e48a
sephturner <turnerj8@wit.edu>
2023-06-28 13:31:05 -0400

    Added styling

--- src/components/PlayerStats.jsx
+++ src/components/PlayerStats.jsx
@@ -1,43 +1,43 @@
 import React from 'react';
 import { Box, Heading, Table, Thead, Tbody, Tr, Th, Td, ChakraProvider } from '@chakra-ui/react';
 
-function PlayerStats({data}) {
+function PlayerStats({ data }) {
   return (
     <ChakraProvider>
-      <Box p={4}>
-        <Heading as="h1" size="lg" p={8} mb={4} textAlign="center">
+      <Box px={8} height="100vh" display="flex" flexDirection="column" justifyContent="top" alignItems="center" backgroundColor="gray.900" color="white" boxShadow="md">
+        <Heading as="h1" size="xl" p={8} mb={4} textAlign="center" color="orange.500">
           {data[0].Player} Statistics
         </Heading>
-        <Table textAlign="center" variant="simple">
+        <Table textAlign="center" variant="striped" colorScheme="gray" size="sm" borderRadius="md">
           <Thead>
             <Tr>
-              <Th>Season</Th>
-              <Th>PTS</Th>
-              <Th>AST</Th>
-              <Th>REB</Th>
-              <Th>TOV</Th>
-              <Th>FG%</Th>
-              <Th>2P%</Th>
-              <Th>3P%</Th>
-              <Th>FT%</Th>
-              <Th>eFG%</Th>
-              <Th>Shooter Grade</Th>
+              <Th color="orange.500" textAlign="center">Season</Th>
+              <Th color="orange.500" textAlign="center">PTS</Th>
+              <Th color="orange.500" textAlign="center">AST</Th>
+              <Th color="orange.500" textAlign="center">REB</Th>
+              <Th color="orange.500" textAlign="center">TOV</Th>
+              <Th color="orange.500" textAlign="center">FG%</Th>
+              <Th color="orange.500" textAlign="center">2P%</Th>
+              <Th color="orange.500" textAlign="center">3P%</Th>
+              <Th color="orange.500" textAlign="center">FT%</Th>
+              <Th color="orange.500" textAlign="center">eFG%</Th>
+              <Th color="orange.500" textAlign="center">Shooter Grade</Th>
             </Tr>
           </Thead>
-          <Tbody>
+          <Tbody backgroundColor="orange.500">
             {data.map((item, index) => (
-              <Tr key={index}>
-                <Td>{item.Season}</Td>
-                <Td>{item.PTS}</Td>
-                <Td>{item.AST}</Td>
-                <Td>{item.REB}</Td>
-                <Td>{item.TOV}</Td>
-                <Td>{item.FGPercentage}%</Td>
-                <Td>{item.TwoPPercentage}%</Td>
-                <Td>{item.ThreePPercentage}%</Td>
-                <Td>{item.FTPercentage}%</Td>
-                <Td>{item.eFGPercentage}%</Td>
-                <Td>{item.ShooterGrade}</Td>
+              <Tr key={index} borderWidth={0} backgroundColor="orange.500">
+                <Td backgroundColor="orange.500" color="gray.900" textAlign="center">{item.Season}</Td>
+                <Td color="gray.900" textAlign="center">{item.PTS}</Td>
+                <Td color="gray.900" textAlign="center">{item.AST}</Td>
+                <Td color="gray.900" textAlign="center">{item.REB}</Td>
+                <Td color="gray.900" textAlign="center">{item.TOV}</Td>
+                <Td color="gray.900" textAlign="center">{item.FGPercentage}%</Td>
+                <Td color="gray.900" textAlign="center">{item.TwoPPercentage}%</Td>
+                <Td color="gray.900" textAlign="center">{item.ThreePPercentage}%</Td>
+                <Td color="gray.900" textAlign="center">{item.FTPercentage}%</Td>
+                <Td color="gray.900" textAlign="center">{item.eFGPercentage}%</Td>
+                <Td color="gray.900" textAlign="center">{item.ShooterGrade}</Td>
               </Tr>
             ))}
           </Tbody>
